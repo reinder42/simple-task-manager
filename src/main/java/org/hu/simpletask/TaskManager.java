593f0bd8e61fc23fd84856b9da19974ff7888445
Reinder de Vries <reinder@interactionauts.com>
2024-02-14 15:12:56 +0100

    Added TaskManager class

---
+++ src/main/java/org/hu/simpletask/TaskManager.java
@@ -0,0 +1,12 @@
+package org.hu.simpletask;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private List<Task> tasks;
+
+    public TaskManager() {
+        this.tasks = new ArrayList<Task>();
+    }
+}
